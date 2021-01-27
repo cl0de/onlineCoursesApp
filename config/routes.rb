82a9d3c36222997ab9d31253825eba1d43e9bac6
clode <uwaclode@gmail.com>
2021-01-27 23:47:23 +0000

    Create Course controller and views files

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  
   root 'pages#index'
   get 'about' => 'pages#about'
+  get 'courses' => 'courses#index' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   
